19a568fc0d4348919b0120dc3b19e4c1fe24f873
albinso <albinso@kth.se>
2017-03-09 20:44:48 +0100

    Reduce duplicate signals

--- receiver.py
+++ receiver.py
@@ -1,12 +1,27 @@
 import RPi.GPIO as GPIO
 from threading import Thread
+import datetime
 
 RECEIVED_SIGNAL = []
 RECEIVE_PIN = 22
 
+codes = ['1010111011101010101010101', '1010111011101010101010111']
+
+last = 5
 def record():
+    beginning_time = datetime.now()
+    cumulative_time = 0
 	while True:
-		RECEIVED_SIGNAL.append(GPIO.input(RECEIVE_PIN))
+		signal = GPIO.input(RECEIVE_PIN)
+		if signal != last:
+			RECEIVED_SIGNAL.append(signal)
+			last = signal
+		cumulative_time = datetime.now() - beginning_time
+		if cumulative_time > EMPTY_RATE:
+			beginning_time = datetime.now()
+
+def parse():
+	pass
 
 def main():
 	thread = Thread(target=record, name='recorder')
